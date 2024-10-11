0111de564d02018e6848781bf2d833ebfecdd326
tiejun <hutiejun@ones.ai>
2024-10-11 15:11:32 +0800

    update to use wasm automerge

--- README.md
+++ README.md
@@ -15,4 +15,8 @@ node v18.15.0 takes 56076ms
 bun 1.0.1 takes 154288ms, and it looks getting slower and slower at each print loop
 
 node v21.7.1 takes 50999ms
+
 bun 1.1.0 takes 29401ms
+
+
+using automerge 2.x wasm, node 22 processed 259778 edits in 5160 ms, bun's performance is similar.
--- automerge-text.js
+++ automerge-text.js
@@ -1,18 +1,24 @@
 // Apply the paper editing trace to an Automerge.Text object, one char at a time
 const { edits, finalText } = require('./editing-trace')
-const Automerge = require('@automerge/automerge')
-
+const { next: Automerge } = require("@automerge/automerge")
 const start = new Date()
-let state = Automerge.from({text: new Automerge.Text()})
+
+let doc = Automerge.from({text: ""})
 
 for (let i = 0; i < edits.length; i++) {
-  if (i % 2000 === 0) console.log(`Processed ${i} edits in ${new Date() - start} ms`)
-  state = Automerge.change(state, doc => {
-    if (edits[i][1] > 0) doc.text.deleteAt(edits[i][0], edits[i][1])
-    if (edits[i].length > 2) doc.text.insertAt(edits[i][0], ...edits[i].slice(2))
+  doc = Automerge.change(doc, doc => {
+    if (edits[i][1] > 0) return Automerge.splice(doc, ["text"], edits[i][0], edits[i][1])
+    if (edits[i].length > 2) return Automerge.splice(doc, ["text"], edits[i][0], edits[i][1], edits[i][2])
   })
 }
+console.log(`Processed ${edits.length} edits in ${new Date() - start} ms`)
 
-if (state.text.join('') !== finalText) {
+if (doc.text !== finalText) {
   throw new RangeError('ERROR: final text did not match expectation')
 }
+
+data = Automerge.save(doc)
+
+// write to file
+const fs = require('fs')
+fs.writeFileSync('automerge-text.data', data)
